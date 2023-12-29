a213d71ad73a1c4966302fbe789282865e5839d8
denys-khr <khrapov.denys@gmail.com>
2023-12-29 17:19:11 +0200

    added faq section and styles

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -39,6 +39,56 @@ function documentOnReady() {
 
 	}
 
+	let acc = document.getElementsByClassName('accordion__title')
+	let accClose = document.getElementsByClassName('accordion__close')
+	let accCount = document.getElementsByClassName('accordion__count')
+	let i
+
+	function closeAllExcept(activeAcc) {
+		for (let j = 0; j < acc.length; j++) {
+			if (acc[j] !== activeAcc) {
+				acc[j].classList.remove('active')
+				acc[j].nextElementSibling.style.maxHeight = null
+				acc[j].closest('.accordion__wrapper').classList.remove('accordion-active')
+			}
+		}
+	}
+
+	for (i = 0; i < acc.length; i++) {
+		acc[i].addEventListener('click', toggleAccordion)
+	}
+
+	for (i = 0; i < accClose.length; i++) {
+		accClose[i].addEventListener('click', toggleAccordion)
+	}
+
+	for (i = 0; i < accCount.length; i++) {
+		accCount[i].addEventListener('click', function () {
+			toggleAccordion.call(this.parentNode.querySelector('.accordion__title'))
+		})
+	}
+
+	function toggleAccordion() {
+		let accordionContent = this.classList.contains('accordion__close') ?
+				this.previousElementSibling.querySelector('.accordion__content') :
+				this.nextElementSibling
+		let isOpen = accordionContent.style.maxHeight
+		let accordionWrapper = this.closest('.accordion__wrapper')
+
+		closeAllExcept(accordionWrapper.querySelector('.accordion__title'))
+		// let imageHolder = document.querySelector('.faq__image-holder');
+		if (isOpen) {
+			accordionWrapper.querySelector('.accordion__title').classList.remove('active')
+			accordionContent.style.maxHeight = null
+			accordionWrapper.classList.remove('accordion-active')
+			// if (imageHolder) imageHolder.classList.remove('active');
+		} else {
+			accordionWrapper.querySelector('.accordion__title').classList.add('active')
+			accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px'
+			accordionWrapper.classList.add('accordion-active')
+			// if (imageHolder) imageHolder.classList.add('active');
+		}
+	}
 }
 
 
