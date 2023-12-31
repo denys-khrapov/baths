5b6bb15dff30db1c368012a4e5e76d295531ed3f
denys-khr <khrapov.denys@gmail.com>
2023-12-31 03:01:12 +0200

    added sel-article section and styles

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', documentOnReady);
 
 function documentOnReady() {
 	initSwiper();
+	initHoverNewsCard();
 
 	function initSwiper() {
 
@@ -24,6 +25,25 @@ function documentOnReady() {
 					},
 				},
 
+			}
+		})
+		let swiperSelArticles = new Swiper('.slider-sel-articles', {
+			mousewheel: true,
+			slidesPerView: 'auto',
+			spaceBetween: 16,
+			loop: false,
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			navigation: {
+				nextEl: '.slider-sel-articles__nav .swiper-button-next',
+				prevEl: '.slider-sel-articles__nav .swiper-button-prev',
+			},
+			breakpoints: {
+				1280: {
+					spaceBetween: 24,
+				},
 			}
 		})
 
@@ -89,6 +109,22 @@ function documentOnReady() {
 			// if (imageHolder) imageHolder.classList.add('active');
 		}
 	}
+
+	function initHoverNewsCard() {
+		let windowWidth = window.innerWidth;
+		if (windowWidth >= 1280) {
+			document.querySelectorAll('.slide-sel-articles').forEach(item => {
+				item.addEventListener('mouseover', function () {
+					this.querySelector('.slide-sel-articles__text-holder-js').style.maxHeight = '126px';
+				});
+
+				item.addEventListener('mouseout', function () {
+					this.querySelector('.slide-sel-articles__text-holder-js').style.maxHeight = '63px';
+				});
+			});
+		}
+	}
+
 }
 
 
